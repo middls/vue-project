dfdd81d2d0d9a7376a86f0d5b608be9744305569
middls <middls647@gmail.com>
2020-02-06 01:26:42 +0300

    Созданы компоненты меню

---
+++ src/components/Auth/AboutUs.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  .content-wrapper
+    section
+      .container
+        h1.ui-title-1 About Us
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
