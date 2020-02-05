ff9ab091fa7d51a8a99ab51d5c2b27fb5621ca38
middls <middls647@gmail.com>
2020-02-05 18:23:44 +0300

    настройка vue-cli и верстка header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  .wrapper
+    header
+      .navbar
+        .container
+          .navbar-content
+            router-link.header-logo(
+              to="/"
+            ) Film Library
+            .button-burger(
+              @click="menuShow = !menuShow"
+              :class="{ active: menuShow }"
+            )
+              span.line.line-1
+              span.line.line-2
+              span.line.line-3
+            .navbar-list__wrapper(
+              :class="{ active: menuShow }"
+            )
+              ul.navbar-list
+                li.navbar-item(
+                  v-for="link in linkMenu"
+                  :key="link.title"
+                  @click="menuShow = !menuShow"
+                )
+                  router-link.navbar-link(
+                    :to="`${link.url}`"
+                  )  {{ link.title }}
+    .content-wrapper
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      menuShow: false,
+      linkMenu: [
+        { title: 'Home', url: '/' },
+        { title: 'Login', url: '/login' },
+        { title: 'Registration', url: '/registration' }
+      ]
+    }
   }
 }
+
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  @import "./assets/style/uinimi.min.css";
 </style>
