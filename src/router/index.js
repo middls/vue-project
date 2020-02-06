5223475e0b54402bf03c412513a11072977e116c
middls <middls647@gmail.com>
2020-02-07 00:50:36 +0300

    реализовал компонент

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,24 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../components/Home'
+import Task from '../components/Task'
 import Registration from '../components/Auth/Registration'
 import Login from '../components/Auth/Login'
-import AboutUs from '../components/Auth/AboutUs'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/task',
+      name: 'task',
+      component: Task
     },
     {
       path: '/registration',
@@ -23,11 +29,6 @@ export default new Router({
       path: '/login',
       name: 'login',
       component: Login
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: AboutUs
     }
   ]
 })
